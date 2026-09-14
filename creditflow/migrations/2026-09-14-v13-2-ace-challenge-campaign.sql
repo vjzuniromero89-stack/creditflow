19a9f7ecf46b2c344e85655b0052ac42b9b644ce
vjzuniromero89-stack <vjzuniromero89@gmail.com>
2026-09-14 18:00:27 -0400

    Add files via upload

---
+++ creditflow/migrations/2026-09-14-v13-2-ace-challenge-campaign.sql
@@ -0,0 +1,7 @@
+-- CreditFlow v13.2 schema/template migration
+-- ALREADY APPLIED to Supabase project hpimqcnkcoqmalslknnn on 2026-09-14.
+-- Keep this file in GitHub for version history.
+-- Migration name: creditflow_v13_2_ace_challenge_campaign
+
+-- Creates aggressive_compliance_campaigns, aggressive_compliance_campaign_events
+-- and ACE templates 60-66. Do not re-run manually unless restoring another database.
